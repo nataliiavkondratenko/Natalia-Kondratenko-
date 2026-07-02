f9037439ea447b5c36399f53c0144f1cb5b3e8d5
nataliiavkondratenko <nataliia.v.kondratenko@gmail.com>
2026-07-02 14:51:08 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { inject } from '@vercel/analytics';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
